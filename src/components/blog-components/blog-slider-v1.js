a79a721b7b9eb6fb7ad36c9967c2decdeb73f8c0
dev <devs@fullbasketproperty.com>
2023-11-17 23:18:47 +0530

    Revert "Blog, review and video added on the home page"
    This reverts commit b349fb6d2dd2f2a18ffa30448329be302a97c5b6.

--- src/components/blog-components/blog-slider-v1.js
+++ src/components/blog-components/blog-slider-v1.js
@@ -1,99 +1,190 @@
 import React, { Component } from 'react';
-import { imgUrl } from "../../utils";
-import { useStaticQuery, graphql } from "gatsby";
-import { format } from 'timeago.js';
-function BlogSlider(props) {
-	const qryData = useStaticQuery(graphql`
-      query BlogData {
-        allStrapiBlog(sort: {Posted_At: DESC}) {
-			nodes {
-			  Title
-			  Description
-			  Posted_At
-			  Image {
-				file {
-				  childImageSharp {
-					gatsbyImageData(height: 562, width: 800, formats: WEBP)
-				  }
-				}
-			  }
-			  Tags {
-				Tag {
-				  Name
-				}
-			  }
-			}
-		}
-      }
-    `);
-	const BlogData = qryData?.allStrapiBlog?.nodes;
-	let PublicUrl = process.env.GATSBY_PUBLIC_URL + '/'
-	let customClass = props.customClass ? props.customClass : ''
-	let sectionClass = props.sectionClass ? props.sectionClass : ''
-	return (
-		<div className={"ltn__blog-area pt-115--- pb-70 go-top " + sectionClass}>
-			<div className="container">
-				<div className="row">
-					<div className="col-lg-12">
-						<div className="section-title-area ltn__section-title-2--- text-center">
-							<h6 className={"section-subtitle ltn__secondary-color " + customClass}>News &amp; Blogs</h6>
-							<h1 className="section-title">{props.title}</h1>
-						</div>
-					</div>
-				</div>
-				<div className="row  ltn__blog-slider-one-active slick-arrow-1 ltn__blog-item-3-normal">
-					{/* Blog Item */}
-					{BlogData &&
-						BlogData.map((blog) => (
-							<div className="col-lg-12">
-								<div className="ltn__blog-item ltn__blog-item-3">
-									<div className="ltn__blog-img">
-										<a href="/blog-details"><img src={imgUrl(blog.Image)} alt="#" /></a>
-									</div>
-									<div className="ltn__blog-brief">
-										<div className="ltn__blog-meta">
-											<ul>
-												{/* <li className="ltn__blog-author">
-													<a href="/team-details"><i className="far fa-user" />by: Admin</a>
-												</li> */}
-												{blog.Tags &&
-													<>
-														<li className="ltn__blog-tags">
-															{
-																blog.Tags.map((tag) => (
-																	<>
-																		<a href="/blog-grid"><i className="fas fa-tags" />{tag?.Tag?.Name}</a>
-																	</>
-																))
 
-															}
-														</li>
-													</>
+import Sidebar from './sidebar';
 
-												}
-
-											</ul>
-										</div>
-										<h3 className="ltn__blog-title"><a href="/blog-details">{blog?.Title}</a></h3>
-										<div className="ltn__blog-meta-btn">
-											<div className="ltn__blog-meta">
-												<ul>
-													<li className="ltn__blog-date"><i className="far fa-calendar-alt" />{format(blog?.Posted_At)}</li>
-												</ul>
-											</div>
-											<div className="ltn__blog-btn">
-												<a href="/blog-details">R	ead more</a>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						))
-					}
-				</div>
-			</div>
+class BlogSlider extends Component {
+  render() {
+    let PublicUrl = process.env.GATSBY_PUBLIC_URL+'/'
+    let customClass = this.props.customClass ? this.props.customClass :''
+    let sectionClass = this.props.sectionClass ? this.props.sectionClass :''
+    return (
+      <div className={ "ltn__blog-area pt-115--- pb-70 go-top "+ sectionClass}>
+		  <div className="container">
+		    <div className="row">
+		      <div className="col-lg-12">
+		        <div className="section-title-area ltn__section-title-2--- text-center">
+		          <h6 className={"section-subtitle ltn__secondary-color "+customClass}>News &amp; Blogs</h6>
+		          <h1 className="section-title">Leatest News Feeds</h1>
+		        </div>
+		      </div>
+		    </div>
+		    <div className="row  ltn__blog-slider-one-active slick-arrow-1 ltn__blog-item-3-normal">
+		      {/* Blog Item */}
+		      <div className="col-lg-12">
+		        <div className="ltn__blog-item ltn__blog-item-3">
+		          <div className="ltn__blog-img">
+		            <a href="/blog-details"><img src={PublicUrl+"assets/img/blog/1.jpg"} alt="#" /></a>
+		          </div>
+		          <div className="ltn__blog-brief">
+		            <div className="ltn__blog-meta">
+		              <ul>
+		                <li className="ltn__blog-author">
+		                  <a href="/team-details"><i className="far fa-user" />by: Admin</a>
+		                </li>
+		                <li className="ltn__blog-tags">
+		                  <a href="/blog-grid"><i className="fas fa-tags" />Interior</a>
+		                  <a href="/blog-grid"><i className="fas fa-tags" />Decorate</a>
+		                </li>
+		              </ul>
+		            </div>
+		            <h3 className="ltn__blog-title"><a href="/blog-details">10 Brilliant Ways To Decorate Your Home</a></h3>
+		            <div className="ltn__blog-meta-btn">
+		              <div className="ltn__blog-meta">
+		                <ul>
+		                  <li className="ltn__blog-date"><i className="far fa-calendar-alt" />June 24, 2021</li>
+		                </ul>
+		              </div>
+		              <div className="ltn__blog-btn">
+		                <a href="/blog-details">Read more</a>
+		              </div>
+		            </div>
+		          </div>
+		        </div>
+		      </div>
+		      {/* Blog Item */}
+		      <div className="col-lg-12">
+		        <div className="ltn__blog-item ltn__blog-item-3">
+		          <div className="ltn__blog-img">
+		            <a href="/blog-details"><img src={PublicUrl+"assets/img/blog/2.jpg"} alt="#" /></a>
+		          </div>
+		          <div className="ltn__blog-brief">
+		            <div className="ltn__blog-meta">
+		              <ul>
+		                <li className="ltn__blog-author">
+		                  <a href="/team-details"><i className="far fa-user" />by: Admin</a>
+		                </li>
+		                <li className="ltn__blog-tags">
+		                  <a href="/blog-grid"><i className="fas fa-tags" />Interior</a>
+		                  <a href="/blog-grid"><i className="fas fa-tags" />Interior</a>
+		                </li>
+		              </ul>
+		            </div>
+		            <h3 className="ltn__blog-title"><a href="/blog-details">The Most Inspiring Interior Design Of 2021</a></h3>
+		            <div className="ltn__blog-meta-btn">
+		              <div className="ltn__blog-meta">
+		                <ul>
+		                  <li className="ltn__blog-date"><i className="far fa-calendar-alt" />July 23, 2021</li>
+		                </ul>
+		              </div>
+		              <div className="ltn__blog-btn">
+		                <a href="/blog-details">Read more</a>
+		              </div>
+		            </div>
+		          </div>
+		        </div>
+		      </div>
+		      {/* Blog Item */}
+		      <div className="col-lg-12">
+		        <div className="ltn__blog-item ltn__blog-item-3">
+		          <div className="ltn__blog-img">
+		            <a href="/blog-details"><img src={PublicUrl+"assets/img/blog/3.jpg"} alt="#" /></a>
+		          </div>
+		          <div className="ltn__blog-brief">
+		            <div className="ltn__blog-meta">
+		              <ul>
+		                <li className="ltn__blog-author">
+		                  <a href="/team-details"><i className="far fa-user" />by: Admin</a>
+		                </li>
+		                <li className="ltn__blog-tags">
+		                  <a href="/blog-grid"><i className="fas fa-tags" />Interior</a>
+		                  <a href="/blog-grid"><i className="fas fa-tags" />Estate</a>
+		                </li>
+		              </ul>
+		            </div>
+		            <h3 className="ltn__blog-title"><a href="/blog-details">Recent Commercial Real Estate Transactions</a></h3>
+		            <div className="ltn__blog-meta-btn">
+		              <div className="ltn__blog-meta">
+		                <ul>
+		                  <li className="ltn__blog-date"><i className="far fa-calendar-alt" />May 22, 2021</li>
+		                </ul>
+		              </div>
+		              <div className="ltn__blog-btn">
+		                <a href="/blog-details">Read more</a>
+		              </div>
+		            </div>
+		          </div>
+		        </div>
+		      </div>
+		      {/* Blog Item */}
+		      <div className="col-lg-12">
+		        <div className="ltn__blog-item ltn__blog-item-3">
+		          <div className="ltn__blog-img">
+		            <a href="/blog-details"><img src={PublicUrl+"assets/img/blog/4.jpg"} alt="#" /></a>
+		          </div>
+		          <div className="ltn__blog-brief">
+		            <div className="ltn__blog-meta">
+		              <ul>
+		                <li className="ltn__blog-author">
+		                  <a href="/team-details"><i className="far fa-user" />by: Admin</a>
+		                </li>
+		                <li className="ltn__blog-tags">
+		                  <a href="/blog-grid"><i className="fas fa-tags" />Interior</a>
+		                  <a href="/blog-grid"><i className="fas fa-tags" />Room</a>
+		                </li>
+		              </ul>
+		            </div>
+		            <h3 className="ltn__blog-title"><a href="/blog-details">Renovating a Living Room? Experts Share Their Secrets</a></h3>
+		            <div className="ltn__blog-meta-btn">
+		              <div className="ltn__blog-meta">
+		                <ul>
+		                  <li className="ltn__blog-date"><i className="far fa-calendar-alt" />June 24, 2021</li>
+		                </ul>
+		              </div>
+		              <div className="ltn__blog-btn">
+		                <a href="/blog-details">Read more</a>
+		              </div>
+		            </div>
+		          </div>
+		        </div>
+		      </div>
+		      {/* Blog Item */}
+		      <div className="col-lg-12">
+		        <div className="ltn__blog-item ltn__blog-item-3">
+		          <div className="ltn__blog-img">
+		            <a href="/blog-details"><img src={PublicUrl+"assets/img/blog/5.jpg"} alt="#" /></a>
+		          </div>
+		          <div className="ltn__blog-brief">
+		            <div className="ltn__blog-meta">
+		              <ul>
+		                <li className="ltn__blog-author">
+		                  <a href="/team-details"><i className="far fa-user" />by: Admin</a>
+		                </li>
+		                <li className="ltn__blog-tags">
+		                  <a href="/blog-grid"><i className="fas fa-tags" />Interior</a>
+		                  <a href="/blog-grid"><i className="fas fa-tags" />Trends</a>
+		                </li>
+		              </ul>
+		            </div>
+		            <h3 className="ltn__blog-title"><a href="/blog-details">7 home trends that will shape your house in 2021</a></h3>
+		            <div className="ltn__blog-meta-btn">
+		              <div className="ltn__blog-meta">
+		                <ul>
+		                  <li className="ltn__blog-date"><i className="far fa-calendar-alt" />June 24, 2021</li>
+		                </ul>
+		              </div>
+		              <div className="ltn__blog-btn">
+		                <a href="/blog-details">Read more</a>
+		              </div>
+		            </div>
+		          </div>
+		        </div>
+		      </div>
+		      {/*  */}
+		    </div>
+		  </div>
 		</div>
-	)
+    )
+  }
 }
 
 export default BlogSlider;
